d265e1764a60b21450c9a7298fe9cac85be33978
akshat <akshatbhargavadeepa@gmail.com>
2018-06-07 19:07:05 +0530

    Added vuesax library and setup basic stuff.

---
+++ src/components/Content.vue
@@ -0,0 +1,14 @@
+<template>
+  <vs-card>
+    <vs-card-header vs-title="Default Card" />
+    <vs-card-body>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </vs-card-body>
+  </vs-card>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+}
+</script>
